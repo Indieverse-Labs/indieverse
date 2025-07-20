64ac988ab775c801d63a7e9b5e10f2ca6a4266e8
Danshin Mikhail <effectdopler0@gmail.com>
2025-07-20 19:07:14 +0300

    feat(faucet): add wallect connect project id

--- apps/faucet/app/_providers/config.ts
+++ apps/faucet/app/_providers/config.ts
@@ -1,11 +1,15 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
+import { cookieStorage, createStorage } from 'wagmi'
 import { hardhat, sepolia } from 'wagmi/chains'
 
 const isDev = process.env.NODE_ENV === 'development'
 
 export const config = getDefaultConfig({
-  appName: 'My RainbowKit App',
-  projectId: 'YOUR_PROJECT_ID',
+  storage: createStorage({
+    storage: cookieStorage,
+  }),
+  appName: 'Faucet',
+  projectId: 'a8af82025fcd4f8403d9e65ad8872736',
   chains: isDev ? [sepolia, hardhat] : [sepolia],
   ssr: true,
 })
